9468ee3d0cd7e0bbec9182ecf23a100710a6e38b
Roberto <ballone.calogero.roberto@gmail.com>
2016-04-24 18:40:07 +0200

    Starting Point

---
+++ EmployeeMonitoringWS/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeMonitoringWS.Models
+{
+    public class Employee
+    {
+        public int id
+        {
+            get;
+            set;
+        }
+
+        [Required(ErrorMessage = "Name is required")]
+        [Display(Name = "Employee Name")]
+        public string name
+        {
+            get;
+            set;
+        }
+
+        [Required(ErrorMessage = "Surname is required")]
+        [Display(Name = "Employee Surname")]
+        public string surname
+        {
+            get;
+            set;
+        }
+    }
+}
