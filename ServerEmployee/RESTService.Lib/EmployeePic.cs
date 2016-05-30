4a556205e6bbc51cc6d15d06e8f3a0b73ceaea7c
marcosenno <swipe-mc@hotmail.it>
2016-05-30 23:03:29 +0200

    v

---
+++ ServerEmployee/RESTService.Lib/EmployeePic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RESTService.Lib
+{
+    public class EmployeePic
+    {
+        public Employee employee {get; set; }
+        public string picture { get; set; }
+    }
+
+}
